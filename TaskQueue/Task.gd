74a53dcc8c0b6de16eb81c2cbf4ea4739a47890c
TheDuriel <manueldurielfischer@gmail.com>
2024-05-16 15:08:43 +0200

    add proper task queue code

--- TaskQueue/Task.gd
+++ TaskQueue/Task.gd
@@ -1,46 +1,91 @@
 class_name Task
 extends RefCounted
 
-signal state_changed(task: Task, state: STATE)
+signal readied(task: Task)
+signal completed(task: Task)
+signal failed(task: Task)
 
-enum STATE {NONE, FAILED, WAITING, READY, DONE}
-var state: STATE:
+enum STATE {NONE, WAITING, READY, COMPLETED, FAILED}
+var state: int:
 	get: return _state
-var _state: STATE = STATE.NONE:
-	set(value):
-		_state = value
-		state_changed.emit(self, _state)
+var _state: int = STATE.NONE
+var _async: bool = false
+var _unique: bool = false
 
 
-## Override this.
+## Override This
 #func _init() -> void:
-
 	#pass
 
-## Override this.
+
+## Override This
 func _prepare() -> void:
-	_ready()
+	goto_ready() #Or if we're waiting, goto_waiting()
 
 
-## Override this.
-func _execute() -> void:
-	_done()
+## Override This
+func _complete() -> void:
+	goto_completed()
+
+
+## Override This
+func _cancel() -> void:
+	goto_failed()
 
 
 func prepare() -> void:
 	_prepare()
 
 
-func execute() -> void:
-	_execute()
+func complete() -> void:
+	_complete()
 
 
-func _fail() -> void: _state = STATE.FAILED
-func _wait() -> void: _state = STATE.WAITING
-func _ready() -> void: _state = STATE.READY
-func _done() -> void: _state = STATE.DONE
+func cancel() -> void:
+	_cancel()
 
-func is_failed() -> bool: return state == STATE.FAILED
-func is_waiting() -> bool: return state == STATE.WAITING
-func is_ready() -> bool: return state == STATE.READY
-func is_done() -> bool: return state == STATE.DONE
+
+func is_async() -> bool:
+	return _async
+
+
+func is_unique() -> bool:
+	return _unique
+
+
+func set_state(new_state: int) -> void:
+	var old_state: int = _state
+	
+	if old_state == new_state:
+		return
+	
+	match new_state:
+		STATE.NONE:
+			pass
+		STATE.WAITING:
+			_state = STATE.WAITING
+			Logger.hint(self, set_state, "waiting")
+		STATE.READY:
+			_state = STATE.READY
+			Logger.hint(self, set_state, "ready")
+			readied.emit(self)
+		STATE.COMPLETED:
+			_state = STATE.COMPLETED
+			Logger.hint(self, set_state, "completed")
+			completed.emit(self)
+		STATE.FAILED:
+			_state = STATE.FAILED
+			Logger.hint(self, set_state, "failed")
+			failed.emit(self)
+
+
+func goto_waiting() -> void: set_state(STATE.WAITING)
+func goto_ready() -> void: set_state(STATE.READY)
+func goto_completed() -> void: set_state(STATE.COMPLETED)
+func goto_failed() -> void: set_state(STATE.FAILED)
+
+func is_waiting() -> bool: return _state == STATE.WAITING
+func is_ready() -> bool: return _state == STATE.READY
+func was_ready() -> bool: return _state > STATE.READY
+func is_completed() -> bool: return _state == STATE.COMPLETED
+func is_failed() -> bool: return _state == STATE.FAILED
--- TaskQueue/TaskQueue.gd
+++ TaskQueue/TaskQueue.gd
@@ -1,73 +1,171 @@
 class_name TaskQueue
 extends RefCounted
 
-var busy: bool:
-	get: return not _tasks.is_empty()
+signal task_queued(task: Task)
+signal task_readied(task: Task)
+signal task_completed(task: Task)
+signal task_failed(task: Task)
+signal task_cancelled(task: Task)
 
+var slow_mode: bool = false
+var slow_mode_wait_time: float = 0.15
+
+var busy: bool:
+	get: return _tasks.is_empty()
+var _tree: SceneTree
 var _tasks: Array[Task] = []
 
 
-func put_first(task: Task) -> void:
-	_tasks.insert(0, task)
-	_flush.call_deferred()
+func _init(tree: SceneTree) -> void:
+	_tree = tree
 
 
-func put_last(task: Task) -> void:
-	_tasks.append(task)
-	_flush.call_deferred()
+func add_task(task: Task) -> void:
+	add_task_to_end(task)
 
 
-func put_before(task: Task, target: Task) -> void:
-	if not target in _tasks:
-		put_last(task)
-		return
+	# Add Task to the start of the queue
+func add_task_to_start(task: Task) -> void:
+	_insert_task_at(task, 0)
+
+
+	# Add Task to the end of the queue
+func add_task_to_end(task: Task) -> void:
+	_insert_task_at(task, _tasks.size())
+
+
+	# Add Task before Task
+func add_task_before(task: Task, before: Task) -> void:
+	var index: int = 0
+	for idx: int in _tasks.size():
+		var value: Task = _tasks[idx]
+		if value == before:
+			index = int(max(idx - 1, 0))
+			break
+	_insert_task_at(task, index)
+
+
+	# Add Task after Task
+func add_task_after(task: Task, after: Task) -> void:
+	var index: int = _tasks.size()
+	for idx: int in _tasks.size():
+		var value: Task = _tasks[idx]
+		if value == after:
+			index = int(min(idx + 1, _tasks.size()))
+			break
+	_insert_task_at(task, index)
+
+
+	# Add Task before all Tasks of Type
+func add_task_before_all(task: Task, task_type: Script) -> void:
+	var index: int = 0
+	for idx: int in _tasks.size():
+		var value: Task = _tasks[idx]
+		if value.get_script() == task_type:
+			index = int(max(idx - 1, 0))
+			break
+	_insert_task_at(task, index)
+
+
+	# Add Task after all Tasks of Type
+func add_task_after_all(task: Task, task_type: Script) -> void:
+	var index: int = _tasks.size()
+	for idx: int in _tasks.size():
+		var value: Task = _tasks[idx]
+		if value.get_script() == task_type:
+			index = int(min(idx + 1, _tasks.size()))
+	_insert_task_at(task, index)
+
+
+func cancel_task(task: Task) -> void:
+	if task in _tasks:
+		_tasks.erase(task)
+		task.cancel()
+		task_cancelled.emit(task)
+
+
+func _insert_task_at(task: Task, index: int) -> void:
+	if task.is_unique():
+		for t: Task in _tasks:
+			if t.get_script() == task.get_script(): # Stupid
+				return
 	
-	var index: int = _tasks.find(target)
-	if index >= _tasks.size() - 1:
-		put_last(task)
-		return
-	if index < 1:
-		put_first(task)
-		return
+	if index == _tasks.size():
+		_tasks.append(task)
+	else:
+		_tasks.insert(index, task)
 	
-	_tasks.insert(index - 1, task)
-	_flush.call_deferred()
-
-
-func put_after(task: Task, target: Task) -> void:
-	if not target in _tasks:
-		put_last(task)
-		return
+	task.readied.connect(_on_task_readied)
+	task.completed.connect(_on_task_completed)
+	task.failed.connect(_on_task_failed)
 	
-	var index: int = _tasks.find(target)
-	if index >=_tasks.size() - 1:
-		put_last(task)
-		return
+	_ready_tasks.call_deferred()
 	
-	_tasks.insert(index + 1, task)
-	_flush.call_deferred()
-
-
-func _on_task_state_changed(task: Task, state: Task.STATE) -> void:
-	match state:
-		Task.STATE.FAILED:
-			_tasks.erase(task)
-		Task.STATE.DONE:
-			_tasks.erase(task)
+	task_queued.emit(task)
 	
-	_flush.call_deferred()
+	Logger.hint(self, _insert_task_at, Logger.get_object_file_name(task) + " - queue size: %s" % _tasks.size())
 
 
-func _flush() -> void:
+func _ready_tasks() -> void:
+	if slow_mode:
+		await _tree.create_timer(slow_mode_wait_time).timeout
+	
+	Logger.hint(self, _ready_tasks)
 	if _tasks.is_empty():
+		Logger.hint(self, _ready_tasks, "nothing to ready.")
 		return
 	
-	var first_task: Task = _tasks[0]
-	match first_task.state:
-		Task.STATE.NONE:
-			first_task.ready()
-		Task.STATE.WAITING:
-			first_task.ready()
+	# All async tasks
+	for t: Task in _tasks:
+		if t.is_async() and t.state == Task.STATE.NONE:
+			t.prepare.call_deferred()
 	
-	if first_task.is_ready():
-		first_task.execute.call_deferred()
+	Logger.hint(self, _ready_tasks, "readying async.")
+	
+	# First task in list
+	var first_task: Task = _tasks[0]
+	
+	Logger.hint(self, _ready_tasks, "First task is: %s" % Logger.get_object_file_name(first_task))
+	
+	if first_task.is_async():
+		Logger.hint(self, _ready_tasks, "first task is async. Stopped")
+		return
+	
+	if first_task.state <= Task.STATE.WAITING:
+		first_task.prepare.call_deferred()
+		Logger.hint(self, _ready_tasks, "readied first task.")
+		return
+	
+	Logger.hint(self, _ready_tasks, "failed. First task is %s" % first_task.state)
+
+
+func _on_task_readied(task: Task) -> void:
+	if slow_mode:
+		await _tree.create_timer(slow_mode_wait_time).timeout
+	
+	if task.is_async() or task == _tasks[0]:
+		task.complete.call_deferred()
+	
+	task_readied.emit(task)
+
+
+func _on_task_completed(task: Task) -> void:
+	if slow_mode:
+		await _tree.create_timer(slow_mode_wait_time).timeout
+	
+	_tasks.erase(task)
+	
+	_ready_tasks.call_deferred()
+	
+	task_completed.emit(task)
+
+
+func _on_task_failed(task: Task) -> void:
+	if slow_mode:
+		await _tree.create_timer(slow_mode_wait_time).timeout
+	
+	_tasks.erase(task)
+	
+	_ready_tasks.call_deferred()
+	
+	task_failed.emit(task)

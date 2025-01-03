43153eab2412a3f59586639cdd29d86c2470bb5a
TheDuriel <manueldurielfischer@gmail.com>
2025-01-03 01:14:13 +0100

    add uilink
    Helper classes for linking UI nodes to automatically reflect properties,
    works in either direction

---
+++ UILink/UILinkController.gd
@@ -0,0 +1,25 @@
+class_name UILinkController
+extends Node
+
+var _controller: Object
+var _target: Control
+var _controller_property: String
+var _target_property: String
+
+
+func _init(controller: Object, target_control: Control, property_id: String, controller_property_name: String) -> void:
+	_controller = controller
+	_target = target_control
+	_target_property = property_id
+	_controller_property = controller_property_name
+	
+	target_control.add_child(self)
+
+
+func _process(_delta: float) -> void:
+	if not _controller:
+		return
+	if not _target:
+		return
+	
+	_target[_target_property] = _controller[_controller_property]
---
+++ UILink/UILinkObserver.gd
@@ -0,0 +1,25 @@
+class_name UILinkObserver
+extends Node
+
+var _observer: Object
+var _target: Control
+var _observer_property: String
+var _target_property: String
+
+
+func _init(observer: Object, target_control: Control, property_id: String, observer_property_name: String) -> void:
+	_observer = observer
+	_target = target_control
+	_target_property = property_id
+	_observer_property = observer_property_name
+	
+	target_control.add_child(self)
+
+
+func _process(_delta: float) -> void:
+	if not _observer:
+		return
+	if not _target:
+		return
+	
+	_observer[_observer_property] = _target[_target_property]

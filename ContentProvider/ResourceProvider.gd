560a01593a7681ef9f389f64a8c31ca61a4e9daa
TheDuriel <manueldurielfischer@gmail.com>
2025-04-14 16:15:37 +0200

    ResourceProvider add caching modes, no longer preload

--- ContentProvider/ResourceProvider.gd
+++ ContentProvider/ResourceProvider.gd
@@ -1,57 +1,72 @@
 class_name ResourceProvider
 extends RefCounted
 
+# NONE = Do not keep a reference to the resource.
+# KEEP = Store a reference to the resource and keep it in memory after loading.
+# PRELOAD = Preload all resources immediately and permanently keep them in memory.
+enum CACHE_MODE {NONE, KEEP, PRELOAD}
+
 const RES_PATH: String = "res://"
 const DOT_PATH: String = "./"
 const REMAP_EXTENSION: String = ".remap"
 
 var _directory: String = ""
 var _extensions: Array[String] = []
-var _resources: Dictionary = {}
+var _resources_paths: Dictionary[String, String] = {}
+var _resources_instances: Dictionary[String, Resource] = {}
 var _load_external: bool = false
+var _cache_mode: CACHE_MODE = CACHE_MODE.NONE
 
 
-func _init(directory: String, file_extensions: Array[String], load_external: bool = false) -> void:
+func _init(directory: String, file_extensions: Array[String], load_external: bool = false, cache_mode: CACHE_MODE = CACHE_MODE.NONE) -> void:
 	_directory = directory
 	_extensions = file_extensions
 	_load_external = load_external
+	_cache_mode = cache_mode
+	
 	_load_resources_from_inside_pck()
 	if _load_external:
 		_load_resources_next_to_binary()
 
 
 func _load_resources_from_inside_pck() -> void:
-	var files_in_dir: PackedStringArray = FileFinder.find(_directory, _extensions)
-	var new_resources: Dictionary = {}
-	for file: String in files_in_dir:
-		new_resources[file.get_file().get_basename()] = load(file)
-	
-	for res: String in new_resources:
-		_resources[res] = new_resources[res]
+	var resource_paths: PackedStringArray = FileFinder.find(_directory, _extensions)
+	for path: String in resource_paths:
+		var resource_id: String = path.get_file().get_basename()
+		_resources_paths[resource_id] = path
+		if _cache_mode == CACHE_MODE.PRELOAD:
+			_resources_instances[resource_id] = load(path)
 
 
 func _load_resources_next_to_binary() -> void:
 	var external_dir: String = _directory.replace(RES_PATH, DOT_PATH)
-	var files_in_dir: PackedStringArray = FileFinder.find(external_dir, _extensions)
-	var new_resources: Dictionary = {}
-	for file: String in files_in_dir:
-		new_resources[file.get_file().get_basename()] = load(file)
-	
-	for res: String in new_resources:
-		_resources[res] = new_resources[res]
+	var resource_paths: PackedStringArray = FileFinder.find(external_dir, _extensions)
+	for path: String in resource_paths:
+		var resource_id: String = path.get_file().get_basename()
+		_resources_paths[resource_id] = path
+		if _cache_mode == CACHE_MODE.PRELOAD:
+			_resources_instances[resource_id] = load(path)
 
 
 func has_resource(name: String) -> bool:
-	return name in _resources
+	return name in _resources_paths
 
 
 func get_resource(name: String) -> Resource:
 	assert(has_resource(name))
-	return _resources[name]
+	
+	if _resources_instances.has(name):
+		return _resources_instances[name]
+	
+	var resource: Resource = load(_resources_paths[name])
+	if _cache_mode == CACHE_MODE.KEEP:
+		_resources_instances[name] = resource
+	
+	return resource
 
 
 func get_count() -> int:
-	return _resources.size()
+	return _resources_paths.size()
 
 
 static func remove_remap(file_path: String) -> String:

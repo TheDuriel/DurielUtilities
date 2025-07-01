f2e43920f123b042c38df8424a3f9b5a19e39689
TheDuriel <manueldurielfischer@gmail.com>
2025-07-01 16:12:25 +0200

    add tweenhelpe

---
+++ Miscellaneous/TweenHelper.gd
@@ -0,0 +1,23 @@
+class_name TweenHelper
+extends RefCounted
+
+
+static func kill(tween: Tween) -> Variant:
+	if tween and tween.is_valid() or tween.is_running():
+		tween.kill()
+	return null
+
+
+static func create(owner: Node,
+		tween_ease: Tween.EaseType = Tween.EASE_IN,
+		tween_trans: Tween.TransitionType = Tween.TRANS_LINEAR) -> Tween:
+	
+	var t: Tween = owner.create_tween()
+	t.set_ease(tween_ease)
+	t.set_trans(tween_trans)
+	return t
+
+
+static func replace(owner: Node, old_tween: Tween) -> Tween:
+	kill(old_tween)
+	return create(owner)

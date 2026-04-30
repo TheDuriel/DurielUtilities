3af7cbc9b05b40d688ee52eeb6506f77a5347f05
TheDuriel <manueldurielfischer@gmail.com>
2026-04-30 22:09:08 +0200

    statemachine now uses abstract classes

--- SimpleStateMachine/SimpleState.gd
+++ SimpleStateMachine/SimpleState.gd
@@ -1,3 +1,4 @@
+@abstract
 class_name SimpleState
 extends RefCounted
 ## Base State Class
@@ -12,16 +13,26 @@ var scene_tree: SceneTree
 
 
 func can_enter_from(_active_state: SimpleState) -> bool:
-	return true
+	return _can_enter_from(_active_state)
 
 
 func can_exit_to(_new_state: SimpleState) -> bool:
-	return true
+	return _can_exit_to(_new_state)
 
 
 func enter(_previous_state: SimpleState) -> void:
-	entered.emit()
+	_enter(_previous_state)
 
 
 func exit(_new_state: SimpleState) -> void:
-	exited.emit()
+	_exit(_new_state)
+
+
+func emit_entered() -> void: entered.emit()
+func emit_exited() -> void: exited.emit()
+
+
+@abstract func _exit(_new_state: SimpleState) -> void
+@abstract func _can_enter_from(_active_state: SimpleState) -> bool
+@abstract func _can_exit_to(_new_state: SimpleState) -> bool
+@abstract func _enter(_previous_state: SimpleState) -> void
--- SimpleStateMachine/SimpleStateNone.gd
+++ SimpleStateMachine/SimpleStateNone.gd
@@ -1,2 +1,18 @@
 class_name SimpleStateNone
 extends SimpleState
+
+
+func _can_enter_from(_active_state: SimpleState) -> bool:
+	return true
+
+
+func _can_exit_to(_new_state: SimpleState) -> bool:
+	return true
+
+
+func _enter(_previous_state: SimpleState) -> void:
+	emit_entered()
+
+
+func _exit(_new_state: SimpleState) -> void:
+	emit_exited()

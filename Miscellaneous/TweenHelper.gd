9a12e98d7371287a5257aad574328d6cdb589801
TheDuriel <manueldurielfischer@gmail.com>
2026-05-20 20:06:03 +0200

    tweenhelper add subtween and finished

--- Miscellaneous/TweenHelper.gd
+++ Miscellaneous/TweenHelper.gd
@@ -21,3 +21,17 @@ static func create(owner: Node,
 static func replace(owner: Node, old_tween: Tween) -> Tween:
 	kill(old_tween)
 	return create(owner)
+
+
+static func add_subtween(owner: Node, tween: Tween) -> Tween:
+	var t: Tween = owner.create_tween()
+	tween.tween_subtween(t)
+	return t
+
+
+static func is_finished(tween: Tween) -> bool:
+	if tween == null:
+		return true
+	if not tween.is_running():
+		return true
+	return false
